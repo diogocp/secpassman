c0a32f6fbc0088fd771d8a44f9be9bcf3cc62b38
Daniel Madruga <madrugadjn@gmail.com>
2017-03-17 16:35:50 +0000

    Add Test functions to GetTest and PutTest

--- client-lib/src/test/java/io/github/diogocp/secpassman/client/PutTest.java
+++ client-lib/src/test/java/io/github/diogocp/secpassman/client/PutTest.java
@@ -1,7 +1,11 @@
 package io.github.diogocp.secpassman.client;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import io.github.diogocp.secpassman.common.Config;
 import io.github.diogocp.secpassman.common.KeyStoreUtils;
+import io.github.diogocp.secpassman.common.messages.PutMessage;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
@@ -11,16 +15,17 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
 import java.security.UnrecoverableKeyException;
+import org.apache.commons.lang3.SerializationUtils;
 import org.junit.Test;
 import org.junit.Assert;
 
 public class PutTest {
 
 
-    KeyPair keyPair;
-    PasswordManager manager;
-    Config config;
-    KeyStore keyStore;
+    private KeyPair keyPair;
+    private PasswordManager manager;
+    private Config config;
+    private KeyStore keyStore;
 
     public PutTest() {
 
@@ -56,7 +61,7 @@ public class PutTest {
             e.printStackTrace();
         }
 
-        String wrong = "wrongpassword";
+        String wrong = "wrongPassword";
 
         byte[] passwordRetrieved = wrong.getBytes();
         try {
@@ -75,4 +80,38 @@ public class PutTest {
         String password2 = new String(passwordRetrieved);
         Assert.assertEquals(password, password2);
     }
+
+    @Test
+    public void noAuthKeyTest() throws IOException{
+        String domain = "tecnico.ulisboa.pt";
+        String username = "client5";
+        String password = "password";
+        HttpClient httpClient = new HttpClient("localhost", 4567);
+
+        PasswordRecord newRecord = new PasswordRecord(domain.getBytes(StandardCharsets.UTF_8),
+                username.getBytes(StandardCharsets.UTF_8),
+                password.getBytes(StandardCharsets.UTF_8));
+        RsaSealedObject<PasswordRecord> sealedRecord;
+
+        try {
+            sealedRecord = new RsaSealedObject<>(newRecord, keyPair.getPublic());
+        } catch (InvalidKeyException | IOException e) {
+            throw new RuntimeException("Failed to encrypt password record", e);
+        }
+
+        final PutMessage message = new PutMessage(keyPair.getPublic(),
+                manager.getHmac(domain.getBytes(StandardCharsets.UTF_8), "domain"),
+                manager.getHmac(username.getBytes(StandardCharsets.UTF_8), "username"),
+                SerializationUtils.serialize(sealedRecord));
+
+        HttpResponse response;
+        try {
+            response = Unirest.post(String.format("http://%s:%d/secpassman", "localhost", 4567))
+                    .body(SerializationUtils.serialize(message))
+                    .asString();
+        } catch (UnirestException e) {
+
+        }
+    }
+
 }
