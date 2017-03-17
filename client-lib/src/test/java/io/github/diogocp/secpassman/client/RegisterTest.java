34f934a659c862d81555a2b79abd2384c906ec27
Daniel Madruga <madrugadjn@gmail.com>
2017-03-17 13:21:52 +0000

    Add Test function messageNotSignedTest to RegisterTest

--- client-lib/src/test/java/io/github/diogocp/secpassman/client/RegisterTest.java
+++ client-lib/src/test/java/io/github/diogocp/secpassman/client/RegisterTest.java
@@ -1,18 +1,18 @@
 package io.github.diogocp.secpassman.client;
 
+import static org.junit.Assert.*;
+
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import io.github.diogocp.secpassman.common.Config;
 import io.github.diogocp.secpassman.common.KeyStoreUtils;
 import io.github.diogocp.secpassman.common.messages.GetMessage;
-import io.github.diogocp.secpassman.server.Main;
 import org.apache.commons.lang3.SerializationUtils;
-import org.junit.BeforeClass;
+import org.apache.http.NoHttpResponseException;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.junit.Assert;
 import java.io.IOException;
 import java.security.*;
 
@@ -75,7 +75,7 @@ public class RegisterTest {
         SignedObject signedMessage = message.sign(priv);
 
         //HttpClient httpClient = new HttpClient("localhost", 4567);
-       // LOG.info("string > {}", config.getPort());
+        // LOG.info("string > {}", config.getPort());
 
         HttpResponse response;
         try {
@@ -86,7 +86,7 @@ public class RegisterTest {
             throw new IOException(e);
         }
 
-        Assert.assertTrue(response.getStatus() ==  400);
+        assertTrue(response.getStatus() == 400);
     }
 
     @Test
@@ -113,8 +113,23 @@ public class RegisterTest {
             throw new IOException(e);
         }
 
-        Assert.assertTrue(response.getStatus() ==  400);
+        assertTrue(response.getStatus() == 400);
     }
 
-    //sendmessagenotsigned
+    @Test
+    public void messageNotSignedTest() {
+        //create message
+        GetMessage message = new GetMessage(keyPair.getPublic(),
+                manager.getHmac("domain".getBytes(), "domain"),
+                manager.getHmac("username".getBytes(), "username"));
+
+        HttpResponse response ;
+        try {
+            response = Unirest.post(String.format("http://%s:%d/secpassman", "localhost", 4567))
+                    .body(SerializationUtils.serialize(message))
+                    .asString();
+        } catch (UnirestException e) {
+
+        }
+    }
 }
