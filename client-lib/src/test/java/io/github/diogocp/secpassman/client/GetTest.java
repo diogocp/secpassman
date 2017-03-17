074b4bab4b06c6ca8a35c4d5fde8c576ee4f2e28
Daniel Madruga <madrugadjn@gmail.com>
2017-03-17 01:21:25 +0000

    Add Tests files in client-lib.
    Tests application API. More tests should be added.

---
+++ client-lib/src/test/java/io/github/diogocp/secpassman/client/GetTest.java
@@ -0,0 +1,77 @@
+package io.github.diogocp.secpassman.client;
+
+
+import io.github.diogocp.secpassman.common.Config;
+import io.github.diogocp.secpassman.common.KeyStoreUtils;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.security.UnrecoverableKeyException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GetTest {
+    KeyPair keyPair;
+    PasswordManager manager;
+    Config config;
+    KeyStore keyStore;
+
+    public GetTest() {
+
+        config = new Config();
+
+        try {
+            keyStore = KeyStoreUtils.loadKeyStore("../secpassman.jks", "jkspass");
+            keyPair = KeyStoreUtils.loadKeyPair(keyStore, "client", "jkspass");
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | UnrecoverableKeyException | InvalidKeyException e) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        manager = new PasswordManager(config.getHost(), Integer.parseInt(config.getPort()));
+        manager.init(keyStore, "client", "jkspass");
+    }
+
+    @Test
+    public void success() {
+        String domain = "tecnico.ulisboa.pt";
+        String username = "client4";
+        String password = "password";
+        try {
+            manager.save_password(domain.getBytes(StandardCharsets.UTF_8),
+                    username.getBytes(StandardCharsets.UTF_8),
+                    password.getBytes(StandardCharsets.UTF_8));
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SignatureException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String wrong = "wrongpassword";
+
+        byte[] passwordRetrieved = wrong.getBytes();
+        try {
+            passwordRetrieved = manager.retrieve_password(domain.getBytes(StandardCharsets.UTF_8),
+                    username.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (SignatureException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String password2 = new String(passwordRetrieved);
+        Assert.assertEquals(password, password2);
+    }
+}
---
+++ client-lib/src/test/java/io/github/diogocp/secpassman/client/RegisterTest.java
@@ -0,0 +1,120 @@
+package io.github.diogocp.secpassman.client;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import io.github.diogocp.secpassman.common.Config;
+import io.github.diogocp.secpassman.common.KeyStoreUtils;
+import io.github.diogocp.secpassman.common.messages.GetMessage;
+import io.github.diogocp.secpassman.server.Main;
+import org.apache.commons.lang3.SerializationUtils;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.junit.Assert;
+import java.io.IOException;
+import java.security.*;
+
+public class RegisterTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RegisterTest.class);
+
+    KeyPair keyPair;
+    PasswordManager manager;
+    Config config;
+    KeyStore keyStore;
+
+    public RegisterTest() {
+
+        config = new Config();
+
+        try {
+            keyStore = KeyStoreUtils.loadKeyStore("../secpassman.jks", "jkspass");
+            keyPair = KeyStoreUtils.loadKeyPair(keyStore, "client", "jkspass");
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | UnrecoverableKeyException | InvalidKeyException e) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        manager = new PasswordManager(config.getHost(), Integer.parseInt(config.getPort()));
+        manager.init(keyStore, "client", "jkspass");
+    }
+
+    @Test
+    public void success() {
+        try {
+            manager.register_user();
+        } catch (IOException | InvalidKeyException e) {
+            LOG.error("test failed", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void RegisterWithRandomPrivateKeyTest() throws Exception {
+        //signed incorrectly
+        // not signed
+        //  signed with other private key;
+        //create message
+        GetMessage message = new GetMessage(keyPair.getPublic(),
+                manager.getHmac("domain".getBytes(), "domain"),
+                manager.getHmac("username".getBytes(), "username"));
+
+        // Get an auth token for this message, to prevent replay attacks
+        message.authToken = manager.getAuthToken(message.uuid);
+
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+        keyGen.initialize(2048, random);
+        KeyPair pair = keyGen.generateKeyPair();
+        PrivateKey priv = pair.getPrivate();
+        LOG.info("private key {}", priv);
+        //PublicKey pub = pair.getPublic();
+
+        // sign with diff key
+        SignedObject signedMessage = message.sign(priv);
+
+        //HttpClient httpClient = new HttpClient("localhost", 4567);
+       // LOG.info("string > {}", config.getPort());
+
+        HttpResponse response;
+        try {
+            response = Unirest.post(String.format("http://%s:%d/secpassman", "localhost", 4567))
+                    .body(SerializationUtils.serialize(signedMessage))
+                    .asString();
+        } catch (UnirestException e) {
+            throw new IOException(e);
+        }
+
+        Assert.assertTrue(response.getStatus() ==  400);
+    }
+
+    @Test
+    public void RegisterWithOthersPrivateKeyTest() throws Exception {
+        KeyPair keyPairUser3 = KeyStoreUtils.loadKeyPair(keyStore, "client3", "jkspass");
+
+        //create message
+        GetMessage message = new GetMessage(keyPair.getPublic(),
+                manager.getHmac("domain".getBytes(), "domain"),
+                manager.getHmac("username".getBytes(), "username"));
+
+        // Get an auth token for this message, to prevent replay attacks, not needed in register
+        message.authToken = manager.getAuthToken(message.uuid);
+
+        // sign with other user's private key
+        SignedObject signedMessage = message.sign(keyPairUser3.getPrivate());
+
+        HttpResponse response;
+        try {
+            response = Unirest.post(String.format("http://%s:%d/secpassman", "localhost", 4567))
+                    .body(SerializationUtils.serialize(signedMessage))
+                    .asString();
+        } catch (UnirestException e) {
+            throw new IOException(e);
+        }
+
+        Assert.assertTrue(response.getStatus() ==  400);
+    }
+
+    //sendmessagenotsigned
+}
