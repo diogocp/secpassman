53d564cc61d8fd0195281e2014f7a3c0fe2598e5
ruitome <rui.tome@tecnico.ulisboa.pt>
2017-04-25 01:30:46 +0100

    Script to generate servers and respective config properties

---
+++ servers.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#number of fault servers
+F=$1
+# numbers of servers = 3f + 1
+N=$((3 * $F + 1))
+LOWER_PORT=4567
+UPPER_PORT=65535
+FOLDER="src/main/resources"
+servers="servers="
+
+if [ $(($N + $LOWER_PORT)) -lt "$UPPER_PORT" ]; then
+	for ((i=1; i<=$N; i++)); do
+		 port=$(($i+$LOWER_PORT))
+		  if [ "$i" -lt "$N" ]; then
+			servers="${servers}localhost:$port,"
+		 else
+			servers="${servers}localhost:$port"
+		 fi
+	done
+	printf "$servers\n" >> "client-lib/$FOLDER/config.properties"
+
+	for ((i=1; i<=$N; i++)); do
+		 port=$(($i+$LOWER_PORT))
+		 mkdir -p server_$i
+		 cp -R "server/" "server_$i/"
+		 echo "host=localhost" >> "server_$i/$FOLDER/config.properties"
+		 echo "port=$port" >> "server_$i/$FOLDER/config.properties"
+		 printf "$servers\n" >> "server_$i/$FOLDER/config.properties"
+	done
+fi
+
