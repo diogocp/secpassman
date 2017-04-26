59a43444cfcf302098d3edaff99281b768f141c9
Diogo Pereira <diogo.pereira@tecnico.ulisboa.pt>
2017-04-26 17:47:25 +0100

    servers.sh: use a temporary subdir for servers

--- servers.sh
+++ servers.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#number of fault servers
+
+# Maximum number of faults that can be tolerated
 F=$1
-# numbers of servers = 3f + 1
+# Total number of servers (n = 3f + 1)
 N=$((3 * $F + 1))
+
 LOWER_PORT=4567
 UPPER_PORT=65535
 FOLDER="src/main/resources"
@@ -10,34 +12,31 @@ servers="servers="
 DIRECTORY="servers.tmp"
 
 if [ $(($N + $LOWER_PORT)) -le "$UPPER_PORT" ]; then
-	for ((i=1; i<=$N; i++)); do
-		 port=$(($i+$LOWER_PORT))
-		  if [ "$i" -lt "$N" ]; then
-			servers="${servers}localhost:$port,"
-		 else
-			servers="${servers}localhost:$port"
-		 fi
-	done
-	printf "$servers\n" > "common/$FOLDER/config.properties"
+    for ((i=1; i<=$N; i++)); do
+        port=$(($i+$LOWER_PORT))
+        if [ "$i" -lt "$N" ]; then
+            servers="${servers}localhost:$port,"
+        else
+            servers="${servers}localhost:$port"
+        fi
+    done
+    printf "$servers\n" > "common/$FOLDER/config.properties"
 
-	if [ -d "$DIRECTORY" ]; then
-	    eval $(rm -rf "$DIRECTORY")
-	    eval $(mkdir -p "$DIRECTORY" && cd "$DIRECTORY")
-    fi
+    rm -r "$DIRECTORY"
+    mkdir -p "$DIRECTORY"
 
-	for ((i=1; i<=$N; i++)); do
-		 port=$(($i+$LOWER_PORT))
-		 cp -R "server/" "server_$i/"
-         cd "server_$i"
-         echo "Starting server $i"
-         eval $(build/install/server/bin/server "$port") &
-         sleep 2
-         cd ".."
-	done
-	echo "Ready"
-	wait
-
+    for ((i=1; i<=$N; i++)); do
+        port=$(($i+$LOWER_PORT))
+        cp -R "server/" "$DIRECTORY/server_$i/"
+        (
+            cd "$DIRECTORY/server_$i"
+            echo "Starting server $i"
+            build/install/server/bin/server "$port"
+        ) &
+        sleep 2
+    done
+    echo "Ready"
+    wait
 else
-	echo "Invalid number of servers"
+    echo "Invalid number of servers"
 fi
-
