2768e95de9b1609857542fe054c5c4b4b8fb27bb
ruitome <rui.tome@tecnico.ulisboa.pt>
2017-05-03 15:07:02 +0100

    Certificates are now removed with gradlew clean

--- servers.sh
+++ servers.sh
@@ -11,17 +11,10 @@ servers="servers="
 DIRECTORY="servers.tmp"
 
 
-#keytool -genkey -alias teste3 -dname "CN=server3, OU=IST, C=PT" -keyalg RSA -validity 365 -keystore xptokeystore.jks -storepass 123456 -keypass 123456
-#keytool -exportcert -alias server3 -rfc -file server3.pem -keystore testekeys.jks -keypass 123456
-
 function clean_up {
     pkill -f "secpassman.server.Main"
 }
 
-# function gen_certs {
-#     keytool -genkey -alias server -dname "CN=$1, OU=IST, C=PT" -keyalg RSA -validity 365 -keystore server.jks -storepass server -keypass server
-#     keytool -exportcert -alias server -rfc -file $1.pem -keystore server.jks -keypass server
-# }
 
 if [ $(($N + $LOWER_PORT)) -le "$UPPER_PORT" ]; then
     trap clean_up SIGTERM SIGINT
